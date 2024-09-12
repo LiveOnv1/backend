afc083ed493ff68ff6965b34347d2cd52d23ca16
junhyeok <jun673012@gmail.com>
2024-09-13 00:04:11 +0900

    init

---
+++ src/main/java/com/chat/liveon/LiveonApplication.java
@@ -0,0 +1,13 @@
+package com.chat.liveon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chat/liveon/LiveonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.liveon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
