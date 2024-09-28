19c5075a1564929d04325296ecd75a1d9bbfbaa5
Junhyeok Seo <jun673012@gmail.com>
2024-09-29 01:34:22 +0900

    feat:cors 설정

--- src/main/java/com/chat/liveon/config/WebConfig.java
+++ src/main/java/com/chat/liveon/config/WebConfig.java
@@ -3,9 +3,20 @@ package com.chat.liveon.config;
 import com.chat.liveon.auth.service.PasswordEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new PasswordEncoder();
