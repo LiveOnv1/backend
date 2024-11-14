d93197996e97227aec9325de282e4e9307ed1b65
Junhyeok Seo <jun673012@gmail.com>
2024-11-14 21:24:32 +0900

    feat:세션 활성화

--- src/main/java/com/chat/liveon/config/WebConfig.java
+++ src/main/java/com/chat/liveon/config/WebConfig.java
@@ -3,18 +3,20 @@ package com.chat.liveon.config;
 import com.chat.liveon.auth.service.PasswordEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
 import org.springframework.session.web.http.CookieSerializer;
 import org.springframework.session.web.http.DefaultCookieSerializer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableSpringHttpSession
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
