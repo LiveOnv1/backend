2c0238615a2b644c58b666ecdbc3049a7dfba7fe
Junhyeok Seo <jun673012@gmail.com>
2024-11-14 21:52:42 +0900

    refactor:세선 어노테이션 삭제, 빈 추가

--- src/main/java/com/chat/liveon/config/WebConfig.java
+++ src/main/java/com/chat/liveon/config/WebConfig.java
@@ -3,14 +3,17 @@ package com.chat.liveon.config;
 import com.chat.liveon.auth.service.PasswordEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
+import org.springframework.session.MapSession;
+import org.springframework.session.MapSessionRepository;
+import org.springframework.session.SessionRepository;
 import org.springframework.session.web.http.CookieSerializer;
 import org.springframework.session.web.http.DefaultCookieSerializer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 @Configuration
-@EnableSpringHttpSession
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
@@ -33,6 +36,11 @@ public class WebConfig implements WebMvcConfigurer {
         serializer.setSameSite("None");
         serializer.setUseSecureCookie(false);
         return serializer;
+    }
+
+    @Bean
+    public SessionRepository<MapSession> sessionRepository() {
+        return new MapSessionRepository(new ConcurrentHashMap<>());
     }
 }
 
